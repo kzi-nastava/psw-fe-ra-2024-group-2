ec49df5a7f223a982057712a6b58342d74ddf0dc
galijander <galicstrahinja@gmail.com>
2024-10-15 13:04:45 +0200

    feat/started work on tour review display and creation

---
+++ Explorer/src/app/feature-modules/layout/tour-reviews/tour-reviews.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xp-tour-reviews',
+  templateUrl: './tour-reviews.component.html',
+  styleUrls: ['./tour-reviews.component.css']
+})
+export class TourReviewsComponent {
+
+}
