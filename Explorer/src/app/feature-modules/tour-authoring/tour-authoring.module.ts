c42c2690f38bb0b0231d9f84f42f251d6702a15b
AleksandarCvijetic <acacvijetic02@gmail.com>
2024-10-16 22:19:02 +0200

    feat: fix

--- Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-authoring.module.ts
@@ -6,6 +6,8 @@ import { EditTourComponent } from './edittour/edittour.component';
 import { ObjectComponent } from './object/object.component';
 import { ObjectFormComponent } from './object-form/object-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CheckpointComponent } from './checkpoint/checkpoint.component';
+import { CheckpointFormComponent } from './checkpoint-form/checkpoint-form.component';
 
 import { AddtourComponent } from './addtour/addtour.component';
 @NgModule({
