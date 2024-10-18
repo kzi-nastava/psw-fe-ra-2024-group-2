29758ec12e39dc91a886e50ce8c21caee24fc98a
AleksandarCvijetic <acacvijetic02@gmail.com>
2024-10-18 20:17:40 +0200

    Fix

--- Explorer/src/app/infrastructure/routing/app-routing.module.ts
+++ Explorer/src/app/infrastructure/routing/app-routing.module.ts
@@ -1,24 +1,23 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from 'src/app/feature-modules/layout/home/home.component';
-import { LoginComponent } from '../auth/login/login.component';
+import { AccountManagementComponent } from 'src/app/feature-modules/administration/account-management/account-management.component';
 import { EquipmentComponent } from 'src/app/feature-modules/administration/equipment/equipment.component';
-import { AuthGuard } from '../auth/auth.guard';
-import { RegistrationComponent } from '../auth/registration/registration.component';
+import { RatingApplicationComponent } from 'src/app/feature-modules/administration/rating-application/rating-application.component';
+import { ToursitClubComponent } from 'src/app/feature-modules/administration/toursit-club/toursit-club.component';
+import { CommentComponent } from 'src/app/feature-modules/blog/comment/comment.component';
+import { HomeComponent } from 'src/app/feature-modules/layout/home/home.component';
 import { ProfileComponent } from 'src/app/feature-modules/stakeholders/profile/profile.component';
-import { MyToursComponent } from 'src/app/feature-modules/tour-authoring/mytours/mytours.component';
+import { RateAppFormComponent } from 'src/app/feature-modules/stakeholders/rate-app-form/rate-app-form.component';
 import { EditTourComponent } from 'src/app/feature-modules/tour-authoring/edittour/edittour.component';
 import { CheckpointComponent } from 'src/app/feature-modules/tour-authoring/checkpoint/checkpoint.component';
 
+import { MyToursComponent } from 'src/app/feature-modules/tour-authoring/mytours/mytours.component';
 import { ObjectComponent } from 'src/app/feature-modules/tour-authoring/object/object.component';
-import { CommentComponent } from 'src/app/feature-modules/blog/comment/comment.component';
 import { TourIssueReportComponent } from 'src/app/feature-modules/tour-execution/tour-issue-report/tour-issue-report.component';
 import { TourReportingComponent } from 'src/app/feature-modules/tour-execution/tour-reporting/tour-reporting.component';
-import { RatingApplicationComponent } from 'src/app/feature-modules/administration/rating-application/rating-application.component';
-import { AccountManagementComponent } from 'src/app/feature-modules/administration/account-management/account-management.component';
-import { ToursitClubComponent } from 'src/app/feature-modules/administration/toursit-club/toursit-club.component';
-import { RateAppFormComponent } from 'src/app/feature-modules/stakeholders/rate-app-form/rate-app-form.component';
-
+import { AuthGuard } from '../auth/auth.guard';
+import { LoginComponent } from '../auth/login/login.component';
+import { RegistrationComponent } from '../auth/registration/registration.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -31,14 +30,7 @@ const routes: Routes = [
   {path: 'checkpoints', component: CheckpointComponent, canActivate: [AuthGuard]},
   {path: 'comment', component: CommentComponent, canActivate: [AuthGuard] },
   {path: 'edittours', component: EditTourComponent, canActivate: [AuthGuard]},
-  {path: 'objects', component: ObjectComponent, canActivate: [AuthGuard]},
-  {path: 'account', component: AccountManagementComponent, canActivate: [AuthGuard]},
-  {path: 'myclub', component: ToursitClubComponent, canActivate: [AuthGuard]},
-  {path: 'tourIssueReport', component: TourIssueReportComponent, canActivate: [AuthGuard]},
-  {path: 'tourReporting', component: TourReportingComponent, canActivate: [AuthGuard]},
-  {path: 'ratingsApplications', component: RatingApplicationComponent, canActivate: [AuthGuard],},
-  {path: 'rateApp', component: RateAppFormComponent, canActivate: [AuthGuard],}
-
+  {path: 'account', component: AccountManagementComponent, canActivate: [AuthGuard],},
 ];
 
 @NgModule({
