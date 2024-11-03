1ddc6a6155905735c3d5b3d0bbd68e7ba5db2dfe
SanjaRadic23 <radic.sanja.rs@gmail.com>
2024-11-03 20:38:35 +0100

    Relosved Enumerations

--- Explorer/src/app/feature-modules/tour-execution/tour-issue-report/tour-issue-report.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/tour-issue-report/tour-issue-report.component.ts
@@ -40,7 +40,7 @@ export class TourIssueReportComponent implements OnInit {
       console.log('Updated Tour Issue Report Names:', this.tourIssueReportName); 
     });
   }
-
+  
   private fetchTourName(report: TourIssueReport): Promise<TourIssueReportName> {
     return new Promise((resolve, reject) => {
       this.service.getById(report.tourId).subscribe({
