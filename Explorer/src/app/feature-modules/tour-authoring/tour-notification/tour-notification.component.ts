3de60dd17f70a75db587c0bec33deb5bb2e431e5
Vojnic <vojnicnemanja@gmail.com>
2024-11-04 12:33:13 +0100

    started making components and stuff

---
+++ Explorer/src/app/feature-modules/tour-authoring/tour-notification/tour-notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xp-tour-notification',
+  templateUrl: './tour-notification.component.html',
+  styleUrls: ['./tour-notification.component.css']
+})
+export class TourNotificationComponent {
+
+}
