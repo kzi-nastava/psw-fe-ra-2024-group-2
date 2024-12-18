fa6c4ce444c813e95857c6c1cf46df2046cf2996
galijander <galicstrahinja@gmail.com>
2024-12-18 14:28:39 +0100

    feat/display level bar in user profile

--- Explorer/src/app/feature-modules/encounters/encounter.service.ts
+++ Explorer/src/app/feature-modules/encounters/encounter.service.ts
@@ -76,7 +76,6 @@ export class EncounterService {
 
   updateUserLevel(userLevelDto: UserLevelDto): Observable<any> {
     const url = `${this.levelUrl}`;
-    console.log(userLevelDto);
     return this.http.put<UserLevelDto>(url, userLevelDto);
   }
   updateHiddenEncounter(encounter: UnifiedEncounterDto): Observable<UnifiedEncounterDto> {
@@ -86,9 +85,13 @@ export class EncounterService {
   updateSocialEncounter(encounter: UnifiedEncounterDto): Observable<UnifiedEncounterDto> {
     return this.http.put<UnifiedEncounterDto>(`${this.apiUrl}/social`, encounter);
   }
+  
+  
   removeUserFromSocialEncounters(id: number | undefined) {
     return this.http.put<UnifiedEncounterDto>(`${this.apiUrl}/removesocial`, id);
   }
+ 
+ 
   getUserLevel(userId: number): Observable<UserLevelDto> {
     const url = `${this.levelUrl}/${userId}`;
     return this.http.get<UserLevelDto>(url);
